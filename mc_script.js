8b60da11f6c3ba6d895bef1f375d879ce0a2cf16
Paige Mabbitt <PMabbitt992@west-mec.org>
2019-01-22 15:17:48 -0700

    Final Commit: Ready for Review
    spacing and layout, detailed CSS for each legend, body text, nav, footer, some mobile styles, register image added and styled validation, comments

--- mc_script.js
+++ mc_script.js
@@ -24,8 +24,8 @@
 window.onload = setForm;
 
 function setForm() {
-   document.forms[0].onsubmit = function() {
-      if (this.checkValidity()) alert("No invalid data detected. Will retain data for further testing.");
-      return false;
-   }
+    document.forms[0].onsubmit = function () {
+        if (this.checkValidity()) alert("Thank you for your order!");
+        return false;
+    }
 }
